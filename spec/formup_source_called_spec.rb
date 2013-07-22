c8719c6376328e7cd021071ff142bc7cf5f1cd9d
pinzolo <pinzolo@gmail.com>
2013-07-22 22:21:41 +0900

    split formup spec

---
+++ spec/formup_source_called_spec.rb
@@ -0,0 +1,67 @@
+# coding: utf-8
+require "spec_helper"
+require "formup"
+
+describe Formup do
+  context "source method called" do
+    before do
+      class TestClassForSourceCalled
+        include Formup
+        source :key1, :attributes => [:attr1, :attr2, :attr3]
+        source :key2, :aliases => {:attr1 => :alias1, :attr2 => :alias2}
+        source :key3, :attributes => [:attr1, :attr2], :aliases => {:attr3 => :alias3, :attr4 => :alias4}
+      end
+    end
+
+    before(:each) do
+      @obj = TestClassForSourceCalled.new
+    end
+
+    describe ".sources" do
+      it "has 3 items" do
+        expect(TestClassForSourceCalled.sources.length).to eq 3
+      end
+
+      it "contains given keys" do
+        expect(TestClassForSourceCalled.sources.key?(:key1)).to eq true
+        expect(TestClassForSourceCalled.sources.key?(:key2)).to eq true
+        expect(TestClassForSourceCalled.sources.key?(:key3)).to eq true
+      end
+
+      it "has values joined key and attributes" do
+        expect(TestClassForSourceCalled.sources[:key1][:attr1]).to eq "key1_attr1"
+        expect(TestClassForSourceCalled.sources[:key1][:attr2]).to eq "key1_attr2"
+        expect(TestClassForSourceCalled.sources[:key1][:attr3]).to eq "key1_attr3"
+        expect(TestClassForSourceCalled.sources[:key3][:attr1]).to eq "key3_attr1"
+        expect(TestClassForSourceCalled.sources[:key3][:attr2]).to eq "key3_attr2"
+      end
+
+      it "has values given by aliases" do
+        expect(TestClassForSourceCalled.sources[:key2][:attr1]).to eq "alias1"
+        expect(TestClassForSourceCalled.sources[:key2][:attr2]).to eq "alias2"
+        expect(TestClassForSourceCalled.sources[:key3][:attr3]).to eq "alias3"
+        expect(TestClassForSourceCalled.sources[:key3][:attr4]).to eq "alias4"
+      end
+    end
+
+    it "define accessor methods by attributes option" do
+      expect(@obj.respond_to?(:key1_attr1)).to eq true
+      expect(@obj.respond_to?(:key1_attr1=)).to eq true
+      expect(@obj.respond_to?(:key1_attr2)).to eq true
+      expect(@obj.respond_to?(:key1_attr2=)).to eq true
+      expect(@obj.respond_to?(:key1_attr3)).to eq true
+      expect(@obj.respond_to?(:key1_attr3=)).to eq true
+      expect(@obj.respond_to?(:key3_attr1)).to eq true
+      expect(@obj.respond_to?(:key3_attr1=)).to eq true
+      expect(@obj.respond_to?(:key3_attr2)).to eq true
+      expect(@obj.respond_to?(:key3_attr2=)).to eq true
+    end
+
+    it "define accessor methods by aliases option" do
+      expect(@obj.respond_to?(:alias1)).to eq true
+      expect(@obj.respond_to?(:alias2)).to eq true
+      expect(@obj.respond_to?(:alias3)).to eq true
+      expect(@obj.respond_to?(:alias4)).to eq true
+    end
+  end
+end
--- spec/formup_spec.rb
+++ spec/formup_spec.rb
@@ -9,33 +9,33 @@ require "active_model/translation"
 describe Formup do
   context "included" do
     before do
-      class TestClass1
+      class TestClassForDefault
         include Formup
       end
-      @obj = TestClass1.new
+      @obj = TestClassForDefault.new
     end
 
     describe ActiveModel::Conversion do
       it "is included" do
-        expect(TestClass1.included_modules.include?(ActiveModel::Conversion)).to eq true
+        expect(TestClassForDefault.included_modules.include?(ActiveModel::Conversion)).to eq true
       end
     end
 
     describe ActiveModel::Validations do
       it "contains ActiveModel::Validations" do
-        expect(TestClass1.included_modules.include?(ActiveModel::Validations)).to eq true
+        expect(TestClassForDefault.included_modules.include?(ActiveModel::Validations)).to eq true
       end
     end
 
     describe ActiveModel::Naming do
       it "is extended" do
-        expect((class << TestClass1; self end).included_modules.include?(ActiveModel::Naming)).to eq true
+        expect((class << TestClassForDefault; self end).included_modules.include?(ActiveModel::Naming)).to eq true
       end
     end
 
     describe ActiveModel::Translation do
       it "is extended" do
-        expect((class << TestClass1; self end).included_modules.include?(ActiveModel::Translation)).to eq true
+        expect((class << TestClassForDefault; self end).included_modules.include?(ActiveModel::Translation)).to eq true
       end
     end
 
@@ -51,79 +51,19 @@ describe Formup do
 
     describe ".source" do
       it "difined" do
-        expect(TestClass1.respond_to?(:source)).to eq true
+        expect(TestClassForDefault.respond_to?(:source)).to eq true
       end
     end
 
     describe ".sources" do
       it "defined" do
-        expect { TestClass1.sources }.not_to raise_error
+        expect { TestClassForDefault.sources }.not_to raise_error
       end
 
       it "returns empty Hash" do
-        expect(TestClass1.sources.length).to eq 0
+        expect(TestClassForDefault.sources.length).to eq 0
       end
     end
   end
 
-  context "source method called" do
-    before do
-      class TestClass2
-        include Formup
-        source :key1, :attributes => [:attr1, :attr2, :attr3]
-        source :key2, :aliases => {:attr1 => :alias1, :attr2 => :alias2}
-        source :key3, :attributes => [:attr1, :attr2], :aliases => {:attr3 => :alias3, :attr4 => :alias4}
-      end
-    end
-
-    before(:each) do
-      @obj = TestClass2.new
-    end
-
-    describe ".sources" do
-      it "has 3 items" do
-        expect(TestClass2.sources.length).to eq 3
-      end
-
-      it "contains given keys" do
-        actual = TestClass2.sources.key?(:key1) && TestClass2.sources.key?(:key2) && TestClass2.sources.key?(:key3)
-        expect(actual).to eq true
-      end
-
-      it "has values joined key and attributes" do
-        expect(TestClass2.sources[:key1][:attr1]).to eq "key1_attr1"
-        expect(TestClass2.sources[:key1][:attr2]).to eq "key1_attr2"
-        expect(TestClass2.sources[:key1][:attr3]).to eq "key1_attr3"
-        expect(TestClass2.sources[:key3][:attr1]).to eq "key3_attr1"
-        expect(TestClass2.sources[:key3][:attr2]).to eq "key3_attr2"
-      end
-
-      it "has values given by aliases" do
-        expect(TestClass2.sources[:key2][:attr1]).to eq "alias1"
-        expect(TestClass2.sources[:key2][:attr2]).to eq "alias2"
-        expect(TestClass2.sources[:key3][:attr3]).to eq "alias3"
-        expect(TestClass2.sources[:key3][:attr4]).to eq "alias4"
-      end
-    end
-
-    it "define accessor methods by attributes option" do
-      expect(@obj.respond_to?(:key1_attr1)).to eq true
-      expect(@obj.respond_to?(:key1_attr1=)).to eq true
-      expect(@obj.respond_to?(:key1_attr2)).to eq true
-      expect(@obj.respond_to?(:key1_attr2=)).to eq true
-      expect(@obj.respond_to?(:key1_attr3)).to eq true
-      expect(@obj.respond_to?(:key1_attr3=)).to eq true
-      expect(@obj.respond_to?(:key3_attr1)).to eq true
-      expect(@obj.respond_to?(:key3_attr1=)).to eq true
-      expect(@obj.respond_to?(:key3_attr2)).to eq true
-      expect(@obj.respond_to?(:key3_attr2=)).to eq true
-    end
-
-    it "define accessor methods by aliases option" do
-      expect(@obj.respond_to?(:alias1)).to eq true
-      expect(@obj.respond_to?(:alias2)).to eq true
-      expect(@obj.respond_to?(:alias3)).to eq true
-      expect(@obj.respond_to?(:alias4)).to eq true
-    end
-  end
 end
