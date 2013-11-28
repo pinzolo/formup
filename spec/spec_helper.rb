15fed1300c05a67eca2c11d9535122d5571f6b1f
pinzolo <pinzolo@gmail.com>
2013-11-28 11:03:08 +0900

    Add filter for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 # coding: utf-8
 require "coveralls"
-Coveralls.wear!
+require "simplecov"
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/bundle/'
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
